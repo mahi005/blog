cc32634f6f2794b37be3f12c36b423bec88ad854
Mahima <maahiisingh3@gmail.com>
2024-12-24 21:14:30 +0530

    blog push

---
+++ OneDrive/Documents/blog/client/src/Post.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    
+<div className="post">
+<div className="image">
+    <img src="https://cdn.pixabay.com/photo/2020/12/25/08/01/doodles-5858809_1280.png" alt="h1"></img>
+ </div>
+
+<div className="texts">
+   <h2>picture of doodles</h2>
+   <p className="info">
+     <a className="author">Dawid pasko</a>
+     <time>2024-12-16 16:45</time>
+   </p>
+   <p className="summary">he href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a lin</p>
+</div>
+
+</div>
+  )
+}
+
+export default Post
